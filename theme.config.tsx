57fbbd7ac978646546c445514f8afc2f3ce1a533
Filip Tibell <filip.tibell@gmail.com>
2023-09-17 13:25:38 -0500

    Add logo to header, fix page seo titles

---
+++ theme.config.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+
+import { useRouter } from "next/router"
+import type { DocsThemeConfig } from "nextra-theme-docs"
+
+const config: DocsThemeConfig = {
+	docsRepositoryBase: "https://github.com/lune-org/docs/blob/main",
+	useNextSeoProps() {
+		const { asPath } = useRouter()
+		if (asPath !== "/") {
+			return {
+				titleTemplate: "%s – Lune",
+			}
+		}
+	},
+	project: {
+		link: "https://github.com/filiptibell/lune",
+	},
+	footer: {
+		text: (
+			<span>
+				MPL-2.0 {new Date().getFullYear()} ©{" "}
+				<a href="https://github.com/filiptibell/lune" target="_blank">
+					Lune
+				</a>
+			</span>
+		),
+	},
+	logo: (
+		<>
+			<img
+				width="40"
+				height="40"
+				src="https://github.com/filiptibell/lune/raw/main/assets/logo/tilt.png"
+			/>
+		</>
+	),
+}
+
+export default config
