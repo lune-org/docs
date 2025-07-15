54da6ff535db59a2fcc23a34c691f62551f02806
Filip Tibell <filip.tibell@gmail.com>
2025-07-15 21:10:24 +0200

    Add base prefix and site url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/docs/",
+  site: "https://lune-org.github.io",
   integrations: [
     starlight({
       title: "Lune",
